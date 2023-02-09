c4f877bec2319601745b75fdaebfdb89dd9893c3
Karan <karanchandekar431@gmail.com>
2023-02-09 11:22:35 +0530

    jsx rules

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 function Greeting() {
-  return <h2>Hello React</h2>
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>Hello React</h2>
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
