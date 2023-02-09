52c5cd68eaa1000940697aa4f3cb79d4ed793764
Karan <karanchandekar431@gmail.com>
2023-02-09 22:03:57 +0530

    Key prop

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 
 const books = [
   {
+    id: 1,
     title: `Little Blue Truck's Valentine`,
     author: 'Alice Schertle',
     img: './images/book-1.jpg',
   },
   {
+    id: 2,
     title: `Atomic Habits`,
     author: 'James Clea',
     img: 'https://m.media-amazon.com/images/I/51B7kuFwQFL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
@@ -19,8 +21,8 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
-      {books.map(({ img, title, author }) => {
-        return <Book img={img} title={title} author={author} />;
+      {books.map(({ img, title, author, id }) => {
+        return <Book img={img} title={title} author={author} key={id} />;
       })}
     </section>
   );
