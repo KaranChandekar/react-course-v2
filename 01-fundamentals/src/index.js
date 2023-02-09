3f330924c3622b7263ea66888cd2fdbd28dba8e0
Karan <karanchandekar431@gmail.com>
2023-02-09 20:10:39 +0530

    Dynamic props

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,26 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-const title = `Little Blue Truck's Valentine`;
-const author = 'Alice Schertle';
-const img = './images/book-1.jpg';
+
+const firstBook = {
+  title: `Little Blue Truck's Valentine`,
+  author: 'Alice Schertle',
+  img: './images/book-1.jpg',
+};
+const secondBook = {
+  title: `Atomic Habits`,
+  author: 'James Clea',
+  img: 'https://m.media-amazon.com/images/I/51B7kuFwQFL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
 const Book = (props) => {
-  console.log(props);
   return (
     <article className="book">
       <img src={props.img} alt={props.title} />
       <h2>{props.title}</h2>
-      <h4>{author}</h4>
+      <h4>{props.author}</h4>
     </article>
   );
 };
