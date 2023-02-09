b1a02028398a144463e34d4201f4536ac11cc98f
Karan <karanchandekar431@gmail.com>
2023-02-09 19:41:50 +0530

    JavaScript in JSX

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import './index.css'
+import './index.css';
 
 const BookList = () => {
   return (
@@ -13,34 +13,23 @@ const BookList = () => {
       <Book />
       <Book />
     </section>
-  )
-}
+  );
+};
+
+const author = 'Alice Schertle';
 
 const Book = () => {
+  const title = `Little Blue Truck's Valentine`;
+
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src="./images/book-1.jpg" alt="Little Blue Truck's Valentine" />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
-  )
-}
+  );
+};
 
-const Image = () => (
-  <img src="./images/book-1.jpg" alt="Little Blue Truck's Valentine" />
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const Title = () => <h2>Little Blue Truck's Valentine</h2>
-
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: '#617d98',
-    fontSize: '0.75rem',
-    marginTop: '0.5rem',
-  }
-  return <h4 style={{ inlineHeadingStyles }}>Alice Schertle</h4>
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(<BookList />)
+root.render(<BookList />);
