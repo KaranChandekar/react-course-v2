1ae995f3d1193ace5726c9425d02e2aaa08fc25c
Karan <karanchandekar431@gmail.com>
2023-02-12 19:21:40 +0530

    Import & Export statements

---
+++ 01-fundamentals/src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    id: 1,
+    title: `Little Blue Truck's Valentine`,
+    author: 'Alice Schertle',
+    img: './images/book-1.jpg',
+  },
+  {
+    id: 2,
+    title: `Atomic Habits`,
+    author: 'James Clea',
+    img: 'https://m.media-amazon.com/images/I/51B7kuFwQFL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  },
+];
--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,45 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-
-const books = [
-  {
-    id: 1,
-    title: `Little Blue Truck's Valentine`,
-    author: 'Alice Schertle',
-    img: './images/book-1.jpg',
-  },
-  {
-    id: 2,
-    title: `Atomic Habits`,
-    author: 'James Clea',
-    img: 'https://m.media-amazon.com/images/I/51B7kuFwQFL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
-  },
-];
+import { books } from './books';
+import Book from './Book';
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
-
   return (
     <section className="booklist">
       {books.map((book) => {
         return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = ({ img, title, author, getBook, id }) => {
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-      <button onClick={() => getBook(id)}>display id</button>
-    </article>
   );
 };
 
