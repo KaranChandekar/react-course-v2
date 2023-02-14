829c7a775424e3fc18d8d68fe71ab9f6697610d5
Karan <karanchandekar431@gmail.com>
2023-02-14 21:30:57 +0530

    Switch to Object

--- 03-advanced-react/src/tutorial/01-useState/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/04-useState-object.jsx
@@ -1,21 +1,26 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  let [name, setName] = useState('peter');
-  let [age, setAge] = useState(24);
-  let [hobby, setHobby] = useState('read books');
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    hobby: 'read books',
+  });
 
   const displayPerson = () => {
-    setName('John');
-    setAge(28);
-    setHobby('scream at the computer');
+    setPerson({
+      name: 'john',
+      age: 28,
+      hobby: 'scream at the computer',
+    });
+    // setPerson({ ...person, name: 'susan' });
   };
 
   return (
     <>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h4>enjoys to: {hobby}</h4>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>enjoys to: {person.hobby}</h4>
       <button className="btn" onClick={displayPerson}>
         show john
       </button>
