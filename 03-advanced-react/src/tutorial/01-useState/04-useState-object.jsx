73a162c909566ee4478fe34095331e7988d3089c
Karan <karanchandekar431@gmail.com>
2023-02-14 21:12:01 +0530

    useState with Object

--- 03-advanced-react/src/tutorial/01-useState/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/04-useState-object.jsx
@@ -1,7 +1,26 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  let [name, setName] = useState('peter');
+  let [age, setAge] = useState(24);
+  let [hobby, setHobby] = useState('read books');
+
+  const displayPerson = () => {
+    setName('John');
+    setAge(28);
+    setHobby('scream at the computer');
+  };
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h4>enjoys to: {hobby}</h4>
+      <button className="btn" onClick={displayPerson}>
+        show john
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
