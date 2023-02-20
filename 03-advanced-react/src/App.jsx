adf1925fce1b9fb68f89e74ff44e797f794608ef
Karan Chandekar <karanchandekar431@gmail.com>
2023-02-20 20:51:47 +0530

    Named Exports

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,10 @@
-import Navbar from './tutorial/04-project-structure/Navbar';
+import { About, Home } from './tutorial/04-project-structure/Pages';
 
 function App() {
   return (
     <div className="container">
-      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
