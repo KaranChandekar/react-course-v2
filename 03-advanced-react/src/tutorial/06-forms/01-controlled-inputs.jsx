6744b6ee5cc57bab125d966632be7eb8a5577a63
Karan Chandekar <karanchandekar431@gmail.com>
2023-02-21 15:36:32 +0530

    Forms Setup

--- 03-advanced-react/src/tutorial/06-forms/01-controlled-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/01-controlled-inputs.jsx
@@ -1,4 +1,17 @@
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  return (
+    <form className="form">
+      <h4>controlled inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">name</label>
+        <input type="text" className="form-input" id="name" />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">email</label>
+        <input type="email" className="form-input" id="email" />
+      </div>
+      <button className="btn btn-block" type="submit">submit</button>
+    </form>
+  );
 };
 export default ControlledInputs;
